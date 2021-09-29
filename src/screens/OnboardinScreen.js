12dc662422525818f5c226ceaf5255256291fbb0
Illyas <illyas.deulect@gmail.com>
2021-09-29 10:21:31 +0530

    initialized starting and login screen

---
+++ src/screens/OnboardinScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const OnboardinScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>Starting Screen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default OnboardinScreen;
