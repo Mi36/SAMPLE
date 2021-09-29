2779f8306c46bf8f5d090d084c3ce9508cffa34a
Illyas <illyas.deulect@gmail.com>
2021-09-29 11:29:13 +0530

    WIP: screens

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
+import Input from '../components/Input';
 
 const LoginScreen = () => {
   return (
     <SafeAreaView>
       <Text>LoginScreen</Text>
+      <Input placeholder={'Email'} />
+      <Input placeholder={'Password'} />
     </SafeAreaView>
   );
 };
--- src/screens/OnboardinScreen.js
+++ src/screens/OnboardinScreen.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
+import Button from '../components/Button';
 
-const OnboardinScreen = () => {
+const OnboardinScreen = ({navigation}) => {
+  const onNavigation = () => navigation.navigate('LOGIN');
   return (
     <SafeAreaView>
       <Text>Starting Screen</Text>
+      <Button label={'Sign In'} onPress={onNavigation} />
     </SafeAreaView>
   );
 };
