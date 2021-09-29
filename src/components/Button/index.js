d69601e179584f0ec91fc3ae6c3f66f80c9f26ac
Illyas <illyas.deulect@gmail.com>
2021-09-29 11:05:54 +0530

    added button component

---
+++ src/components/Button/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+
+const Button = ({label, onPress}) => {
+  return (
+    <TouchableOpacity style={styles.main} onPress={onPress} activeOpacity={0.8}>
+      <Text style={styles.label}>{label}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default Button;
+
+Button.propTypes = {
+  label: PropTypes.string,
+  onPress: PropTypes.func,
+};
+
+Button.defaultProps = {
+  label: 'PRESS HERE',
+  onPress: null,
+};
+
+const styles = StyleSheet.create({
+  main: {
+    height: 60,
+    backgroundColor: 'green',
+    alignItems: 'center',
+    borderRadius: 10,
+    justifyContent: 'center',
+  },
+  label: {
+    fontSize: 25,
+  },
+});
