0572b97b52e974f5cc5bc53a58922090d60995ce
Illyas <illyas.deulect@gmail.com>
2021-09-29 11:28:17 +0530

    added Input component

---
+++ src/components/Input/index.js
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import {StyleSheet, TextInput} from 'react-native';
+
+const Input = ({onChange, value, placeholder}) => {
+  return (
+    <TextInput
+      style={styles.input}
+      onChangeText={onChange}
+      value={value}
+      placeholder={placeholder}
+    />
+  );
+};
+
+export default Input;
+
+Input.propTypes = {
+  placeholder: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+};
+
+Input.defaultProps = {
+  placeholder: 'Enter',
+  value: null,
+  onChange: null,
+};
+
+const styles = StyleSheet.create({
+  input: {
+    height: 60,
+    borderWidth: 1,
+    marginTop: 20,
+    paddingLeft: 15,
+    borderRadius: 10,
+  },
+});
