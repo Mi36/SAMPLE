04d05e8823127bdb95634d02b2757691969a4a11
Illyas <illyas.deulect@gmail.com>
2021-09-30 13:16:58 +0530

    added thunk middleware

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Router from './src/common/router';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import ReduxThunk from 'redux-thunk';
 
 const App = () => {
   return <Router />;
