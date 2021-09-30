0c4be27a99335e21ef3347d83528e7d6e01ad27c
Illyas <illyas.deulect@gmail.com>
2021-09-30 16:28:08 +0530

    added dummy users

---
+++ data/dummy-data.js
@@ -0,0 +1,5 @@
+export const users = [
+  {email: 'test@test1.com', password: '1234561'},
+  {email: 'test@test2.com', password: '1234562'},
+  {email: 'test@test3.com', password: '1234563'},
+];
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import {SafeAreaView, Text} from 'react-native';
 import {useDispatch} from 'react-redux';
+import {users} from '../../data/dummy-data';
 import Button from '../components/Button';
 import Input from '../components/Input';
 import KeyboardAvoidingViewWrapper from '../components/KBAvoidinView.js';
@@ -16,6 +17,7 @@ const LoginScreen = ({navigation}) => {
   const [passwordError, setPasswordError] = useState(null);
 
   const onLogin = () => {
+    setEmailError(null);
     const emailRegex =
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (email && !emailRegex.test(email.toLowerCase())) {
@@ -32,7 +34,10 @@ const LoginScreen = ({navigation}) => {
     }
     if (!emailError && !passwordError) {
       //store to redux ang logged inside
-      if (email === 'test@test.com' && password === '123456') {
+      const existingUser = users.find(user => {
+        return user.email === email && user.password === password;
+      });
+      if (existingUser) {
         dispatch(setUser(email, password));
         navigation.navigate('MAIN_STACK');
       } else {
