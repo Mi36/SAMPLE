fc87b20b35e7a6d39170b9adb0e35f7d5305dec4
Illyas <illyas.deulect@gmail.com>
2021-09-30 19:03:04 +0530

    dummy data updated

--- data/dummy-data.js
+++ data/dummy-data.js
@@ -3,3 +3,52 @@ export const users = [
   {email: 'test@test2.com', password: '1234562'},
   {email: 'test@test3.com', password: '1234563'},
 ];
+
+export const products = [
+  {
+    id: '1',
+    name: 'Pen & Paper',
+    image: require('../assets/penpaper.png'),
+    description:
+      'Can be used for role-playing (not the kind of role-playing you are thinking about...).',
+    price: 250,
+  },
+  {
+    id: '2',
+    name: 'PowerBook',
+    image: require('../assets/laptop.png'),
+    description:
+      'Awesome hardware, crappy keyboard and a hefty price. Buy now before a new one is released!',
+    price: 2500,
+  },
+  {
+    id: '3',
+    name: 'The Book - Limited Edition',
+    image: require('../assets/book.png'),
+    description:
+      "What the content is? Why would that matter? It's a limited edition!",
+    price: 15.99,
+  },
+  {
+    id: '4',
+    name: 'Coffee Mug',
+    image: require('../assets/mug.png'),
+    description: 'Can also be used for tea!',
+    price: 8.99,
+  },
+  {
+    id: '5',
+    name: 'Blue Carpet',
+    image: require('../assets/carpet.jpeg'),
+    description:
+      'Sifa Carpet Kashmiri Persian Silk Carpet for Living Room with 1 Inch Thickness ( 6x9 Feet Cream Color)',
+    price: 99.99,
+  },
+  {
+    id: '6',
+    name: 'Red Shirt',
+    image: require('../assets/shirt.png'),
+    description: 'A red t-shirt, perfect for days with non-red weather.',
+    price: 29.99,
+  },
+];
