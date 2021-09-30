93ca42a1a5c84b2074a4d1c12b858ffbe6336eee
Illyas <illyas.deulect@gmail.com>
2021-09-30 12:36:06 +0530

    initialized styles

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -2,14 +2,18 @@ import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
 import Button from '../components/Button';
 import Input from '../components/Input';
+import KeyboardAvoidingViewWrapper from '../components/KBAvoidinView.js';
+import styles from '../styles/loginScreen';
 
 const LoginScreen = ({navigation}) => {
   return (
-    <SafeAreaView>
-      <Text>LoginScreen</Text>
-      <Input placeholder={'Email'} />
-      <Input placeholder={'Password'} />
-      <Button onPress={() => navigation.navigate('MAIN_STACK')} />
+    <SafeAreaView style={styles.safeAreaView}>
+      <KeyboardAvoidingViewWrapper>
+        <Text>LoginScreen</Text>
+        <Input placeholder={'Email'} />
+        <Input placeholder={'Password'} />
+        <Button onPress={() => navigation.navigate('MAIN_STACK')} />
+      </KeyboardAvoidingViewWrapper>
     </SafeAreaView>
   );
 };
