0a8f5c1f1ffe9a0cfc599646e0a33dbd52fca9a6
Illyas <illyas.deulect@gmail.com>
2021-09-30 19:51:46 +0530

    update in button component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {StyleSheet, Text, TouchableOpacity} from 'react-native';
 
-const Button = ({label, onPress}) => {
+const Button = ({label, onPress, style}) => {
   return (
-    <TouchableOpacity style={styles.main} onPress={onPress} activeOpacity={0.8}>
+    <TouchableOpacity
+      style={[styles.main, style]}
+      onPress={onPress}
+      activeOpacity={0.8}>
       <Text style={styles.label}>{label}</Text>
     </TouchableOpacity>
   );
@@ -15,23 +18,26 @@ export default Button;
 Button.propTypes = {
   label: PropTypes.string,
   onPress: PropTypes.func,
+  style: PropTypes.object,
 };
 
 Button.defaultProps = {
   label: 'PRESS HERE',
   onPress: null,
+  style: null,
 };
 
 const styles = StyleSheet.create({
   main: {
     height: 60,
-    backgroundColor: 'green',
+    backgroundColor: '#F55352',
     alignItems: 'center',
     borderRadius: 10,
     justifyContent: 'center',
     marginTop: 20,
   },
   label: {
-    fontSize: 25,
+    fontSize: 20,
+    fontWeight: '700',
   },
 });
